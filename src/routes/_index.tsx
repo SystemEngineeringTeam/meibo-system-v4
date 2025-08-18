4f4b3aa7b9950d5e0e9edade68bd4d5e017bcd87
masa <massanndesu52@gmail.com>
2025-08-19 01:17:02 +0900

    add: exampleを追加した

--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -1,10 +1,36 @@
 import type { ReactElement } from "react";
-import { styled as p } from "panda/jsx";
+import { css } from "panda/css";
+import { Flex, styled as p } from "panda/jsx";
 
+const oreorep = css({
+  "& > p": {
+    _hover: { bg: "mv4-error", transform: "translateY(-1px)" },
+    bg: "mv4-primary",
+    color: "mv4-onPrimary",
+    px: "10",
+    transition: "all 0.2s",
+  },
+});
 export default function (): ReactElement {
   return (
-    <p.div color="mv4-primary">
-      やあ
-    </p.div>
+    <Flex
+      className={oreorep}
+      flexDir={{
+        base: "column",
+        md: "row",
+      }}
+      gap="100"
+    >
+
+      <p.p>
+        やあ
+      </p.p>
+      <p.p>
+        やあ
+      </p.p>
+      <p.p>
+        やあ
+      </p.p>
+    </Flex>
   );
 }
