c19f51f3c7830806782b7b370db04923c5bc8c7d
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-11-10 23:49:01 +0900

    MemberTableコンポーネントを新しいtable.tsxファイルに移動し、route.tsxでのインポートパスを更新

--- src/routes/members/route.tsx
+++ src/routes/members/route.tsx
@@ -1,11 +1,11 @@
 import type { ColumnDef } from "@tanstack/react-table";
 import type { JSX } from "react";
-import type { MemberData } from "@/components/MemberTable";
+import type { MemberData } from "@/components/table";
 import { useCallback, useMemo, useState } from "react";
 import IconMaterialSymbolsArrowDropDown from "~icons/material-symbols/arrow-drop-down";
 
 import IconMaterialSymbolsArrowDropUp from "~icons/material-symbols/arrow-drop-up";
-import MemberTable from "@/components/MemberTable";
+import MemberTable from "@/components/table";
 
 export default function Members(): JSX.Element {
   const icon = "https://nenex.me/assets/ira-D6gCFlkL.png";
